7bcdfeb56a411b80febd319de2acef90ea08549d
Isa Setiawan Abdurrazaq <azaqokay@gmail.com>
2016-09-22 20:48:25 +0700

    Update isa.py
    representasi telah diubah

--- transferSGtoEC/isa.py
+++ transferSGtoEC/isa.py
@@ -2,36 +2,55 @@ import random
 def fangsi(val):
     x=val[0]
     y=val[1]
-    return 99999 - ((x+(2*y)-7)**2) + ((((2*x)+y-5))**2)
+    a = 0.0000000000001 #mencegah pembagian nol
+    GEDE = 9999999
+    # fungsi minimasi bukat 1/h-a
+    h = ((x+(2*y)-7)*(x+(2*y)-7)) + ((((2*x)+y-5))*(((2*x)+y-5)))
+    return GEDE - h
 
 def fitness(individu):
-    return fangsi(decode(individu))
+    return fangsi(genotofeno(individu))
 
-def encode(x,y):
-    xbin = '{0:05b}'.format(((1 << 5) - 1) & x)
-    ybin = '{0:05b}'.format(((1 << 5) - 1) & y)
-    return xbin+ybin
 
 def decode(code):
-    return twocomplement(code[0:5]),twocomplement(code[5:10])
+    # representasi telah diganti 
+    ra = 10
+    rb = -10
+    N = len(code)
+    c = 0
+    a = 0
 
-def twocomplement(a):
-    x=int(a,2)
-    if (x>(2**len(a)-1)-(2**(len(a)-1))):
-        return x - (1 << len(a))
+    for i in xrange(1,N+1):
+        c = c + (2**(-i))
+
+    for g in xrange(0,N):
+        if (code[g]=="1"): 
+            a += (2**(-(g+1)))
+
+    return rb + ((ra-rb)*a/c)
+
+def genotofeno(kromosom):
+    # menampilkan fentipe dari genotipe satu individu
+    if (len(kromosom)%2==1):
+        print "panjang kromosom harus genap woy"
+        return 0
     else:
-        return x
+        x,y = kromosom[:len(kromosom)/2], kromosom[len(kromosom)/2:]
+        return decode(x),decode(y)
 
-def randomindv():
+
+def randomindv(panjangkromosom):
+    # panjangkromosom harus genap siah
     ret = ""
-    for i in range(0,10):
+    for i in range(0,panjangkromosom):
         ret = ret + `random.randint(0,1)`
     return ret
 
-def ranpopul(l):
+
+def ranpopul(jumlahindividu,panjangkromosom):
     a = []
-    for i in range(0,l):
-        a += [randomindv()]
+    for i in range(0,jumlahindividu):
+        a += [randomindv(panjangkromosom)]
     return a
 
 # def rodaputar(popul):
@@ -39,6 +58,7 @@ def ranpopul(l):
 #     sumfit = 0;
 #     for p in popul:
 #         sumfit += fangsi(decode(p))
+
 
 def listprobabi(popul):
     #mengembalikan list probabilitas kumulatif
@@ -55,6 +75,7 @@ def rodaputar(popul,probs,n):
             if r <= probs[i]:
                 terpilih.append(indv)
                 break
+
     return terpilih
 
 def crossover(indv1,indv2):
@@ -62,6 +83,7 @@ def crossover(indv1,indv2):
     anak1 = indv1[:t] + indv2[t:]
     anak2 = indv2[:t] + indv1[t:]
     return [anak1,anak2]
+
 
 def mutasi(individu):
     pm = 1/len(individu) #referensi dari wikiperdia https://en.wikipedia.org/wiki/Mutation_(genetic_algorithm)
@@ -77,12 +99,19 @@ def mutasi(individu):
             hasilmutasi += iv
     return hasilmutasi
 
-pop = 16 #banyaknya populasi
-peru = 100 #jumlah perulangan
-popo = ranpopul(pop)
+kromo = 10
+pop = 10 #banyaknya populasi
+loop = 100 #jumlah perulangan
+popo = ranpopul(pop,kromo)
+
+# for i in popo:
+#     print genotofeno(i),"   |",fitness(i)
+
+
+
 print [(t) for t in popo]
 i = 0
-while i<peru:
+while i<loop:
     ortu = rodaputar(popo,listprobabi(popo),pop);
     anakanak = []
     for j in range(0,pop/2):
@@ -91,8 +120,8 @@ while i<peru:
     popo = mutan
     fitneses = [decode(p) for p in popo]
     print "generasi ke ",i
-    for g in fitneses:
-        print g, fangsi(g)
+    for individu in popo:
+        print genotofeno(individu)
     i += 1
 
 
